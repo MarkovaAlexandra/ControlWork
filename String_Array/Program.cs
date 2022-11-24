8731cdc7aa67a3aeacbf58bd64bbb7c2a2f1b400
Alexandra <matildakrokodilda@gmail.com>
2022-11-24 12:51:44 +0300

    проверка if count ==0

--- String_Array/Program.cs
+++ String_Array/Program.cs
@@ -1,6 +1,4 @@
-﻿
-
-string[] MaxLengthElement (string[]Array, int dlina)
+﻿string[] MaxLengthElement (string[]Array, int dlina)
 {
 
 int count=0;
@@ -9,22 +7,22 @@ for (int i = 0; i < Array.Length; i++)
     if (Array[i].Length<dlina)
     count++;
 }
+    string [] NewArray = new string [count];
+    int j=0;
+    for (int i = 0; i < Array.Length; i++)
+        {
+            if (Array[i].Length<dlina)
+                {
+                    NewArray[j]=Array[i];
+                    j++;
+                }
+        }
 
-string [] NewArray = new string [count];
-int j=0;
-for (int i = 0; i < Array.Length; i++)
-{
-    if (Array[i].Length<dlina)
-    {
-        NewArray[j]=Array[i];
-       // System.Console.WriteLine($"{j}= {NewArray[j]}");
-        j++;
-    }
-}
-//System.Console.WriteLine(string.Join(",",NewArray));
+if (count==0)System.Console.WriteLine("таких нет");
 return NewArray;
 }
 string [] ArrayInput = new string [] {"csharp", "123", "git", "Forest", "hi", "nine","bye","6=6", "6!=2"};
-int dlina = 6;
+System.Console.WriteLine("Введите максимальную длину элемента");
+int dlina = Convert.ToInt32(Console.ReadLine());
 string [] ArrayResult = MaxLengthElement(ArrayInput,dlina);
 System.Console.WriteLine(string.Join(",",ArrayResult));
