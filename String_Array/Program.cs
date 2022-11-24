d892287768e3a83c3eb08bd522d23869531c1280
Alexandra <matildakrokodilda@gmail.com>
2022-11-24 12:17:55 +0300

    first commit

---
+++ String_Array/Program.cs
@@ -0,0 +1,21 @@
+﻿string [] Array = new string [] {"csharp", "123", "git", "Forest", "hi", "nine","bye","6=6", "6!=2"};
+
+int count=0;
+for (int i = 0; i < Array.Length; i++)
+{
+    if (Array[i].Length<4)
+    count++;
+}
+
+string [] NewArray = new string [count];
+int j=0;
+for (int i = 0; i < Array.Length; i++)
+{
+    if (Array[i].Length<4)
+    {
+        NewArray[j]=Array[i];
+        System.Console.WriteLine($"{j}= {NewArray[j]}");
+        j++;
+    }
+}
+System.Console.WriteLine(string.Join(",",NewArray));
